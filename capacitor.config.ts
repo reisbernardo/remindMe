4b231b4303e30dc1ae01eed91c4b969155d8d095
reisbernardo <bernardo.sereis@gmail.com>
2022-11-18 19:57:18 -0300

    agora rodando o app angular em android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'remind-me',
+  webDir: 'docs',
+  bundledWebRuntime: false
+};
+
+export default config;
